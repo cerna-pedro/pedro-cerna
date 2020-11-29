cc3fabf65b665db5c7a15f8bd5598d5db8ab2646
Pedro Cerna <pedro.d.cerna@gmail.com>
2020-11-29 14:11:23 -0600

    Added heat map.

--- src/lib/projects.js
+++ src/lib/projects.js
@@ -8,47 +8,51 @@ export const projects = [
     url: "https://www.riskofunk.com/",
   },
   {
-    name: 'Southland HVAC Services',
-    url:'https://southlandhvacserv.com/'
+    name: "Southland HVAC Services",
+    url: "https://southlandhvacserv.com/",
   },
   {
-    name: 'Color of the Year, Confetti',
-    url:'https://cerna-pedro.github.io/color-confetti/'
+    name: "Color of the Year, Confetti",
+    url: "https://cerna-pedro.github.io/color-confetti/",
   },
   {
-    name: 'Wedding Countdown',
-    url: 'https://cerna-pedro.github.io/wedding-countdown/'
+    name: "Wedding Countdown",
+    url: "https://cerna-pedro.github.io/wedding-countdown/",
   },
   {
-    name: 'Kaaaaranavaaairus',
-    url: 'https://cerna-pedro.github.io/Kaaaaranavaaairus/'
+    name: "Kaaaaranavaaairus",
+    url: "https://cerna-pedro.github.io/Kaaaaranavaaairus/",
   },
   {
-    name: 'Music Collection Finder',
-    url: 'https://music-finder.netlify.app/'
+    name: "Music Collection Finder",
+    url: "https://music-finder.netlify.app/",
   },
   {
     name: `What's My Current Weather?`,
-    url: 'https://cerna-pedro.github.io/my-weather/'
+    url: "https://cerna-pedro.github.io/my-weather/",
   },
   {
-    name: 'Pomodoro Timer',
-    url:'https://cerna-pedro.github.io/pomodoro/'
+    name: "Pomodoro Timer",
+    url: "https://cerna-pedro.github.io/pomodoro/",
   },
   {
-    name: 'Agent Jason Cruz Listings',
-    url:'https://cruz-listings.netlify.app/'
+    name: "Agent Jason Cruz Listings",
+    url: "https://cruz-listings.netlify.app/",
   },
   {
-    name: 'Photo Shoot',
-    url:'https://photo-shoot.netlify.app/'
+    name: "Photo Shoot",
+    url: "https://photo-shoot.netlify.app/",
   },
   {
-    name: 'United States GDP, 1947-2015',
-    url:'https://cerna-pedro.github.io/us-gdp/'
+    name: "United States GDP, 1947-2015",
+    url: "https://cerna-pedro.github.io/us-gdp/",
   },
   {
-    name: 'Doping in Professional Bicycle Racing',
-    url:'https://cerna-pedro.github.io/bike-doping/'
+    name: "Doping in Professional Bicycle Racing",
+    url: "https://cerna-pedro.github.io/bike-doping/",
+  },
+  {
+    name: "Monthly Global Land-Surface Temperature",
+    url: "https://cerna-pedro.github.io/global-heat/",
   },
 ]
