18377194f46434cf660e58f3d659418cdd6445fb
Pedro Cerna <48631243+cerna-pedro@users.noreply.github.com>
2025-08-10 18:49:06 -0500

    Update links.js

--- src/lib/links.js
+++ src/lib/links.js
@@ -3,18 +3,8 @@ import { FaLinkedin, FaGithub, FaInstagram } from "react-icons/fa"
 
 export const links = [
   {
-    name: "GitHub",
-    url: "https://github.com/cerna-pedro",
-    icon: <FaGithub/>,
-  },
-  {
     name: "LinkedIn",
     url: "https://www.linkedin.com/in/pedro-cerna/",
     icon: <FaLinkedin/>,
-  },
-  {
-    name: "Instagram",
-    url: "https://www.instagram.com/pedro_cerna_/",
-    icon: <FaInstagram/>,
   },
 ]
